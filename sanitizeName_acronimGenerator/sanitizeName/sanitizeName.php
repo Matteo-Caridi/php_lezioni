a01cf28e2c4e8eb3fdf10ad9c1cc01a783377072
Matteo-Caridi <77775829+Matteo-Caridi@users.noreply.github.com>
2021-02-23 18:06:32 +0100

    Add files via upload

--- sanitizeName_acronimGenerator/sanitizeName/sanitizeName.php
+++ sanitizeName_acronimGenerator/sanitizeName/sanitizeName.php
@@ -2,31 +2,24 @@
 
 function sanitizeName($name)
 {
-
-    // $noString = strip_tags($name);
-    $deleteScript = preg_replace('#<script(.*?)>(.*?)</script>#is', ' ', $name);
-    $noString = filter_var($deleteScript, FILTER_SANITIZE_STRING);
-    $tinyName = preg_replace('/[^a-z .A-Z]+/', '', $noString);
+    $noScript = preg_replace('#<script(.*?)>(.*?)</script>#is','',$name);
+    $noString = filter_var($noScript, FILTER_SANITIZE_STRING);
+    $tinyName = preg_replace('/[^a-z .A-Z]+/m', '', $noString);
     $explodename = explode(' ', $tinyName);
     $correctNames = array_map('correctCase', $explodename);
     $joinString = implode(' ', $correctNames);
 
 
-
-    $noSpaceBetween = preg_replace('/[ ]+/', ' ', $joinString);
-    $noSpaceEnd = trim($noSpaceBetween);
-
-    return $noSpaceEnd;
+    $noSpaceBetween = preg_replace('/[ ]+/m', ' ', $joinString);
+    $noSpace = trim($noSpaceBetween);
+    return $noSpace;
 }
 
 
 function correctCase($name)
 {
 
-
-    // il/i nomi tipo 'aAa' | 'AAA' con strtolower diventano --> 'aaa' | 'aaa'
     $nameLowercase = strtolower($name);
-    // se abbiamo tipo 'aaa' con ucfirst diventa --> 'Aaa
     $uppercasename = ucfirst($nameLowercase);
 
     return $uppercasename;
--- sanitizeName_acronimGenerator/sanitizeName/sanitizeNameTest.php
+++ sanitizeName_acronimGenerator/sanitizeName/sanitizeNameTest.php
@@ -1,30 +1,30 @@
 <?php
 
 // Il comando php ... viene eseguito dalla root del progetto
-require 'sanitizeName_acronimGenerator/sanitizeName/sanitizeName.php';
+require 'sanitizeName.php';
 
 // require './sanitizeName.php';
 
 $dataset = [
-    ['mario','Mario',__LINE__],
-    ['mAriO','Mario',__LINE__],
-    ['MARIO','Mario',__LINE__],
-    ['De giovanni','De Giovanni',__LINE__],
-    ['de giovanni','De Giovanni',__LINE__],
-    ['de Giovanni','De Giovanni',__LINE__],
-    ['de Giovanni      ','De Giovanni',__LINE__],
-    ['de                     Giovanni      ','De Giovanni',__LINE__],
-    ['de    Giovanni      ','De Giovanni',__LINE__],
-    ['de 55 Giovanni','De Giovanni',__LINE__],
-    ['Mario83','Mario',__LINE__],
-    ['Mario@','Mario',__LINE__],
-    ['Mario@     ','Mario',__LINE__],
-    ['John Romita Sr.','John Romita Sr.',__LINE__],
-    ['John Romita Jr.','John Romita Jr.',__LINE__],
-    ['John Romita Jr.','John Romita Jr.',__LINE__],
-    ['<h1>John123456789</h1>','John',__LINE__],
-    ['<script>alert("ciccio")</script>','',__LINE__],
-    ['    <h1>   John123456789   </h1>   ','John',__LINE__],
+    ['mario', 'Mario', __LINE__],
+    ['mAriO', 'Mario', __LINE__],
+    ['MARIO', 'Mario', __LINE__],
+    ['De giovanni', 'De Giovanni', __LINE__],
+    ['de giovanni', 'De Giovanni', __LINE__],
+    ['de Giovanni', 'De Giovanni', __LINE__],
+    ['de Giovanni      ', 'De Giovanni', __LINE__],
+    ['de                     Giovanni      ', 'De Giovanni', __LINE__],
+    ['de    Giovanni      ', 'De Giovanni', __LINE__],
+    ['de 55 Giovanni', 'De Giovanni', __LINE__],
+    ['Mario83', 'Mario', __LINE__],
+    ['Mario@', 'Mario', __LINE__],
+    ['Mario@     ', 'Mario', __LINE__],
+    ['John Romita Sr.', 'John Romita Sr.', __LINE__],
+    ['John Romita Jr.', 'John Romita Jr.', __LINE__],
+    ['John Romita Jr.', 'John Romita Jr.', __LINE__],
+    ['<h1>John123456789</h1>', 'John', __LINE__],
+    ['<script>alert("ciccio")</script>', '', __LINE__],
+    ['    <h1>   John123456789   </h1>   ', 'John', __LINE__],
 
 ];
 
@@ -33,16 +33,13 @@ foreach ($dataset as $key => $row) {
     $atteso = $row[1];
     $line = $row[2];
 
-
     $result = sanitizeName($text);
 
 
 
     if ($result == $atteso) {
-        echo "Test passato - tutto ok nella linea: $line \n";
+        echo "Test passato - tutto ok \n";
     } else {
-        // var_dump($result);
-
-        echo "Test NON passato nella linea: $line \n";
+        echo "Test NON passato, linea: $line \n";
     }
 }
