a4eec5236ba59ba384171fc039d76ab60fb74031
Matteo Caridi <caridimatteo0@outlook.it>
2021-02-25 13:25:36 +0100

    vista istruzione json_decode

--- case-study/tasklistArray/file_get_contents.php
+++ case-study/tasklistArray/file_get_contents.php
@@ -1,5 +1,17 @@
+
 <?php
 
     $jsonString = file_get_contents('case-study\tasklistArray\dataset\TaskList.json');
-    var_dump($jsonString);
+   
+    $jsonArray = json_decode($jsonString, true);
+
+    // var_dump($jsonString);
+
+
+    // echo gettype($jsonString);
+
+    // esempio per stampare valore dell'array associativo
+    print_r($jsonArray[17]['expirationDate']);
+
+
 ?>
