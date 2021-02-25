55a83c31a3db56c8fb68b6dc3860445f2bdc81d2
Matteo Caridi <caridimatteo0@outlook.it>
2021-02-25 10:20:45 +0100

    due modi per stampare

--- Topic/string_basic/php_html.php
+++ Topic/string_basic/php_html.php
@@ -1,12 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$colors = ['red', 'green', 'blue'];
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        ul {
+            text-align: center;
+            border: 1px solid red;
+            width: 200px;
+        }
+    </style>
+</head>
 
-?>
+<body>
+    <?php
 
-<ul>
-    <?php foreach ($colors as $color) { ?>
-        <li><?php echo $color ?></li>
+    $colors = ['red', 'green', 'blue'];
 
-    <?php } ?>
-</ul>
+    ?>
+
+
+    <ul>
+        <?php foreach ($colors as $color) { ?>
+            <li><?php echo $color ?> </li>
+        <?php } ?>
+    </ul>
+
+    <ul>
+        <?php foreach ($colors as $color) {
+            echo "<li>$color</li>";
+        } ?>
+    </ul>
+</body>
+
+</html>
