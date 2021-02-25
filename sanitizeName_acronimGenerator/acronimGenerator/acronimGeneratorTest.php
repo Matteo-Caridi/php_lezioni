07296d5d20ff183097fb058d8a53da7f3bbd0c9c
Matteo Caridi <caridimatteo0@outlook.it>
2021-02-25 10:07:07 +0100

    si commmit

---
+++ sanitizeName_acronimGenerator/acronimGenerator/acronimGeneratorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'acronimGenerator.php';
+
+$dataset = [
+    ['gioco molto difficile', 'G.M.D.', __LINE__],
+    ['General Motors', 'G.M.', __LINE__],
+    ['Federal Bureau Investigation', 'F.B.I.', __LINE__],
+];
+
+foreach ($dataset as $key => $row) {
+
+    $text = $row[0];
+    $atteso = $row[1];
+    $line = $row[2];
+
+    $result = acronimGenerator($text);
+
+
+    if ($result === $atteso) {
+        echo 'Test Superato alla linea: $line';
+        var_dump($result);
+
+    } else {
+        echo 'Test Fallito alla liinea: $line';
+        var_dump($result);
+    }
+}
