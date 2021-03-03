96786f63596370a358c709563152d6d5a25cd4d5
Matteo Caridi <caridimatteo0@outlook.it>
2021-03-03 11:49:37 +0100

    fix if su base isset'error'

--- case-study/tasklistArray/lib/JSONReaderTest.php
+++ case-study/tasklistArray/lib/JSONReaderTest.php
@@ -1,38 +1,38 @@
 <?php
-
+/**
+ * NOTA: Il test deve essere lanciato dalla cartella lib
+ */
 declare(strict_types=1);
-
 system("clear");
-require 'case-study\tasklistArray\lib\JSONReader.php';
-require 'vendor\testTools\testTool.php';
+require 'JSONReader.php';
+require 'vendor/testTools/testTool.php';
 
 
 $dataset = [
-
     [
+        'description' => 'Gestione dell\'errore per file inesistente',
         'filepath' => './ciccio.json',
-        'error' => 'file not exist',
-        'errorCode' => 404,
+        'error' => 'not-exists',
+        'errorCode' => 404
     ],
-
     [
+        'description' => 'Lettura dei dati di un array di 20 elementi',
         'filepath' => 'case-study\tasklistArray\dataset\TaskList.json',
         'type' => 'array',
-        'count' => 20,
-
+        'count' => 20
     ],
     [
+        'description' => 'Gestione file con errori di sintassi',
         'filepath' => 'case-study\tasklistArray\dataset\TaskList-error.json',
         'error' => 'syntax-error',
-        'errorCode' => 123,
+        'errorCode' => 123
     ],
     [
+        'description' => 'Lettura di un file con un array vuoto',
         'filepath' => 'case-study\tasklistArray\dataset\TaskList-zero.json',
         'type' => 'array',
-        'count' => 0,
+        'count' => 0
     ],
-
-
 
 ];
 
@@ -43,12 +43,15 @@ foreach ($dataset as $row) {
         $actual = JSONReader($filepath);
 
 
-        if (isset($row['type'])) {
+        if (!isset($row['error'])) {
+
             $type = $row['type'];
-            assertEquals('array', gettype($actual), 'ottengo un array');
-            if (count($actual) === $row['count']) {
-                echo "stesso numero di elementi: " . $row['count'] . "\n";
-            };
+            assertEquals($type, gettype($actual), 'tipo di dato');
+            assertEquals($row['count'], count($actual), 'numero di elementi');
+        } else {
+            assertEquals($row['error'], null, 'era attesa un eccezione');
+            assertEquals($row['errorCode'], null, 'era attesa un eccezione');
+
         }
     } catch (Exception $e) {
         echo 'Caught exception: ' . $e->getMessage() . ' ' . $e->getCode() . ' ' .   "line: " . $e->getLine() . "\n";
