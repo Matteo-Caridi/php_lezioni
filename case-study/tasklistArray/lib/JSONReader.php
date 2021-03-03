61a106d9887a6b8f70565ab1a4ebd1903acd3f63
Matteo Caridi <caridimatteo0@outlook.it>
2021-03-03 13:25:20 +0100

    fix

--- case-study/tasklistArray/lib/JSONReader.php
+++ case-study/tasklistArray/lib/JSONReader.php
@@ -1,25 +1,26 @@
+  
 <?php
-
 /**
  * Apre un file formato JSON e lo converte in array associativo
  * 
- * @var string $filepath percorso del file da leggere
- * @return array $res restituisce array associativo corrispondente
+ * @var    string $filepath percorso del file da leggere
+ * @return array $res array associativo corrispondente
  */
-
-function JSONReader(string $filepath): ?array
+function JSONReader(string $filepath)
 {
-    if (file_exists($filepath)) {
-
+    if(file_exists($filepath))
+    {
         $fileString = file_get_contents($filepath);
         $res = json_decode($fileString, true);
-        $code = json_last_error();
-        $msg = json_last_error_msg();
-        if ($code  != JSON_ERROR_NONE) {
-            throw new Exception($msg, $code);
-        }
+        $code =json_last_error();
+        if($code != JSON_ERROR_NONE )
+        {
+            throw new Exception('syntax-error', $code);
+        } 
+    
     } else {
-        throw new Exception('file not exist', 404);
+        throw new Exception('not-exists', 404); 
     }
+
     return $res;
 }
--- case-study/tasklistArray/lib/JSONReaderTest.php
+++ case-study/tasklistArray/lib/JSONReaderTest.php
@@ -3,10 +3,12 @@
  * NOTA: Il test deve essere lanciato dalla cartella lib
  */
 declare(strict_types=1);
-system("clear");
-require 'JSONReader.php';
-require 'vendor/testTools/testTool.php';
+require "../../../vendor/testTools/testTool.php";
+require "./JSONReader.php";
 
+system("clear");
+
+echo testHeader(__FILE__);
 
 $dataset = [
     [
@@ -17,45 +19,52 @@ $dataset = [
     ],
     [
         'description' => 'Lettura dei dati di un array di 20 elementi',
-        'filepath' => 'case-study\tasklistArray\dataset\TaskList.json',
+        'filepath' => '../dataset/TaskList.json',
         'type' => 'array',
         'count' => 20
     ],
     [
         'description' => 'Gestione file con errori di sintassi',
-        'filepath' => 'case-study\tasklistArray\dataset\TaskList-error.json',
+        'filepath' => '../dataset/TaskList-error.json',
         'error' => 'syntax-error',
-        'errorCode' => 123
+        'errorCode' => 4,
     ],
     [
         'description' => 'Lettura di un file con un array vuoto',
-        'filepath' => 'case-study\tasklistArray\dataset\TaskList-zero.json',
+        'filepath' => '../dataset/TaskList-zero.json',
         'type' => 'array',
         'count' => 0
     ],
 
 ];
 
-foreach ($dataset as $row) {
+foreach ($dataset as $key => $row) {
+
+    echo datasetHeader($key, $dataset);
 
     try {
+        
         $filepath = $row['filepath'];
         $actual = JSONReader($filepath);
-
-
-        if (!isset($row['error'])) {
-
+        
+        if (!isset($row['error']))
+        {
             $type = $row['type'];
+           
             assertEquals($type, gettype($actual), 'tipo di dato');
             assertEquals($row['count'], count($actual), 'numero di elementi');
+        
         } else {
+
             assertEquals($row['error'], null, 'era attesa un eccezione');
             assertEquals($row['errorCode'], null, 'era attesa un eccezione');
-
+            
         }
+    
     } catch (Exception $e) {
-        echo 'Caught exception: ' . $e->getMessage() . ' ' . $e->getCode() . ' ' .   "line: " . $e->getLine() . "\n";
-        // var_dump($e->getMessage() == $row['error']);
-        // var_dump($e->getCode() == $row['errorCode']);
+
+            assertEquals($e->getMessage(), $row['error'], 'messaggio di errore');
+            assertEquals($e->getCode(), $row['errorCode'], 'codice di errore');
+
     }
-};
+}
