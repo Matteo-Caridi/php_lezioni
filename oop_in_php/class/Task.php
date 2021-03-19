d7919e8452f5a160b4e2d1fbe4642b7fd67d38a0
Matteo-Caridi <caridimatteo0@outlook.it>
2021-03-19 16:24:57 +0100

    task con metodo isExpired

---
+++ oop_in_php/class/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class Task
+{
+    public $id;
+    public $taskName;
+    public $status;
+    public $expirationDate;
+
+    public function isExpired():bool
+    {
+        //istanza della classe DateTime
+        $today = new DateTime();
+        //print_r(gettype($today)); --> ci darà object
+        //get_class($today) --> DateTime
+        $task = new DateTime($this->expirationDate);
+        //se vero allora la task è scaudata
+        //è una valutazione di verità
+        return $task>$today;
+    }
+
+    public function getExpirationDate()
+    {
+        return $this->expirationDate;
+    }
+}
