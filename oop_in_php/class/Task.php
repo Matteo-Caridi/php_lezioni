a45bf68fb94f103461c7597969784721880dc9cf
Matteo-Caridi <caridimatteo0@outlook.it>
2021-03-30 15:33:33 +0200

    refactoring

--- oop_in_php/class/Task.php
+++ oop_in_php/class/Task.php
@@ -8,21 +8,25 @@ class Task
     public $status;
     public $expirationDate;
 
-    public function isExpired(): bool
+    public function isExpired(DateTime $expiration): bool
     {
         //istanza della classe DateTime
         $today = new DateTime();
                 
         //print_r(gettype($today)); --> ci darà object
         //get_class($today) --> DateTime
-
-        $task = new DateTime($this->expirationDate);
-     
-        if($today->format('Ymd')===$task-> format('Ymd')){
-            return false;
+        try {
+            $task = new DateTime($this->expirationDate);
+            //verifichiamo se la scadenza è oggi o no
+            if($expiration->format('Ymd')===$task-> format('Ymd')){
+                return false;
+            }
+    
+            return ($today->getTimestamp()>$task->getTimestamp());
+            
+        } catch (\Throwable $th) {
+            return $th;
         }
-
-        return ($today->getTimestamp()>$task->getTimestamp());
     
     }
 
--- oop_in_php/test/class/TaskIsExpiredTest.php
+++ oop_in_php/test/class/TaskIsExpiredTest.php
@@ -4,18 +4,24 @@ require '.\vendor\testTools\testTool.php';
 require '.\class\Task.php';
 
 $dataset = [
-    ['2021-03-29', true, 'task scaduta ieri'],
-    ['2021-03-30', false, 'task di oggi'],
-    ['2021-03-31', false, 'task che scade domani'],
+    ['2021-03-29', '2021-03-30', true, 'task scaduta ieri'],
+    ['2021-03-30', '2021-03-30', false, 'task di oggi'],
+    ['2021-03-31', '2021-03-30', false, 'task che scade domani'],
 ];
 
 foreach ($dataset as $testCase) {
-    list($inputDate, $expected, $description) = $testCase;
+    list($inputDate, $today, $expected, $description) = $testCase;
 
     $task = new Task();
     $task->taskName = 'Task';
     $task->expirationDate = $inputDate;
     $task->status = 'done';
-    
-    assertEquals($expected, $task->isExpired(),$description);
+
+    try {
+        $todayDateTime = new DateTime($today);
+        assertEquals($expected, $task->isExpired($todayDateTime), $description);
+        assertEquals('boolean', gettype($expected), "il risultato dev'essere un booleano");
+    } catch (\Throwable $th) {
+        assertEquals('Expectation',$expected, 'Eccezione per formato errato');
+    }
 }
